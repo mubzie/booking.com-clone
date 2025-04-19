710cb3ac3dcd940eeca1c42565044c60cd9e4cb5
mubzie <rabiumubarak92@gmail.com>
2025-04-19 18:54:57 +0100

    feat: enhance Header component with product listings and call-to-action buttons

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,12 @@ import React, { useState } from "react";
 import Link from "next/link";
 import {
   Bars3Icon,
+  ChatBubbleLeftIcon,
   ChevronDownIcon,
+  HomeIcon,
+  PaperAirplaneIcon,
+  PhoneIcon,
+  PlayCircleIcon,
   XMarkIcon,
 } from "@heroicons/react/20/solid";
 import {
@@ -13,8 +18,37 @@ import {
   Popover,
   PopoverButton,
   PopoverGroup,
+  PopoverPanel,
   Transition,
 } from "@headlessui/react";
+import { icons } from "lucide-react";
+import { it } from "node:test";
+
+const products = [
+  {
+    name: "Book a stay",
+    description: "Get a better understanding of your traffic",
+    href: "#",
+    icon: HomeIcon,
+  },
+  {
+    name: "Book a flight",
+    description: "Speak directly to your customers",
+    href: "#",
+    icon: PaperAirplaneIcon,
+  },
+  {
+    name: "Contact our Support Team",
+    description: "Your customers's data will be safe and secure",
+    href: "#",
+    icon: ChatBubbleLeftIcon,
+  },
+];
+
+const callToActions = [
+  { name: "See Demo Booking", href: "#", icons: PlayCircleIcon },
+  { name: "Contact Support", href: "#", icons: PhoneIcon },
+];
 
 function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -51,6 +85,45 @@ function Header() {
                 aria-hidden="true"
               />
             </PopoverButton>
+            <Transition
+              as={React.Fragment}
+              enter="transition east-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <PopoverPanel className="absolute z-10 mt-3 w-screen top-full -left-8 max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+                <div className="p-4">
+                  {products.map((item) => (
+                    <div
+                      key={item.name}
+                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
+                    >
+                      <div className="flex h-11 w-11 flex-none justify-center items-center rounded-lg bg-gray-50 group-hover:bg-gray-200">
+                        <item.icon
+                          className="h-6 w-6 text-[#013b94] group-hover:text-blue-600"
+                          aria-hidden="true"
+                        />
+                      </div>
+                      <div className="flex-auto">
+                        <a
+                          href={item.href}
+                          className="block font-semibold text-[#013b94]"
+                        >
+                          {item.name}
+                          <span className="absolute inset-0"></span>
+                        </a>
+                        <p className="mt-1 text-[#013b94]">
+                          {item.description}
+                        </p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </PopoverPanel>
+            </Transition>
           </Popover>
         </PopoverGroup>
       </nav>
